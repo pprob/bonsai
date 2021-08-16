d64842713a623da3cfd65065c8676c29ed35f64a
pprob <p.probert340@gmail.com>
2021-08-16 18:58:05 +0100

    refactor: clean up

--- src/client/containers/App/index.tsx
+++ src/client/containers/App/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import Routes from './Routes';
+import Header from '../../components/Header';
 import '../../styles/_global.scss';
 
-const App = () => <Routes />;
+const App: React.FC = () => (
+  <>
+    <Header />
+    <Routes />
+  </>
+);
 
 export default App;
