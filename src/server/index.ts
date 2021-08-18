22337e5b0e67afa7b309177aa423aef166a2cede
pprob <p.probert340@gmail.com>
2021-08-18 23:14:41 +0100

    feat: mongo connection

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,32 @@
 import https from 'https';
 import app from './app';
+import mongoose from 'mongoose';
 
-console.log(process.env.HTTPS);
-const {HTTPS, PORT} = process.env;
+const {HTTPS, PORT, MONGO_URI} = process.env;
+
+const connect = async () => {
+  if (!MONGO_URI) {
+    throw new Error('MongoDB URI must be defined');
+  }
+
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 const startServer = () => {
   const listener = () => {
     console.log(`Server listening on port ${PORT}`);
   };
+
+  connect();
 
   if (HTTPS === 'true') {
     const options = {
