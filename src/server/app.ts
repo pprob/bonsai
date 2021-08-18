345cf30bee1860da63220f62bf466d2aee1b20aa
pprob <p.probert340@gmail.com>
2021-08-18 23:19:02 +0100

    chore: add cookie session

--- src/server/app.ts
+++ src/server/app.ts
@@ -6,6 +6,8 @@ import 'express-async-errors';
 import pageRouter from './pages/index';
 import staticRouter from './config/staticRouter';
 import configRouter from './config/configRouter';
+import cookieSession from 'cookie-session';
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware';
 
 const assetsPath = path.join(__dirname, '../../dist');
 
@@ -17,11 +19,18 @@ app
   .engine('html', ejs.renderFile)
   .use(json())
   .use(cookieParser())
+  .use(
+    cookieSession({
+      signed: false,
+      secure: process.env.NODE_ENV !== 'development',
+    }),
+  )
   .use(staticRouter)
   .use('/config', configRouter)
   .use('/api', (req, res) => {
     res.send('hello');
   })
   .get('*', pageRouter);
+app.use(errorHandlerMiddleware);
 
 export default app;
