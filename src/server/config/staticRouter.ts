a85173f20b1c57988a3fb1b9278621a5e0d3f51f
pprob <p.probert340@gmail.com>
2021-08-18 23:15:07 +0100

    chore: clean up

--- src/server/config/staticRouter.ts
+++ src/server/config/staticRouter.ts
@@ -3,7 +3,7 @@ import path from 'path';
 const router = express.Router();
 
 const assetsPath = path.join(__dirname, '../../../dist');
-console.log(assetsPath);
+
 router
   .use(express.static(assetsPath))
   .get('/hearbeat', (req: Request, res: Response) => {
