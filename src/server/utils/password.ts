138bde8bd188c26e5d68b92ea1341fece17f1d6e
pprob <p.probert340@gmail.com>
2021-08-19 00:06:10 +0100

    refactor: update password util to prevent timing attacks

--- src/server/utils/password.ts
+++ src/server/utils/password.ts
@@ -1,25 +1,30 @@
 import {promisify} from 'util';
-import {scrypt, randomBytes} from 'crypto';
+import {scrypt, randomBytes, timingSafeEqual} from 'crypto';
 
 const scryptAsync = promisify(scrypt);
 
 class PasswordUtil {
   static async hashPassword(password: string): Promise<string> {
     const salt = randomBytes(32).toString('hex');
-    const buffer = (await scryptAsync(password, salt, 64)) as Buffer;
+    const derivedKey = (await scryptAsync(password, salt, 64)) as Buffer;
 
-    return `${buffer.toString('hex')}.${salt}`;
+    return `${derivedKey.toString('hex')}:${salt}`;
   }
 
   static async comparePassword(
     storedPassword: string,
     suppliedPassword: string,
   ): Promise<boolean> {
-    const [hashedPassword, salt] = storedPassword.split('.');
+    const [hashedPassword, salt] = storedPassword.split(':');
 
-    const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+    const keyBuffer = Buffer.from(hashedPassword, 'hex');
+    const derivedKey = (await scryptAsync(
+      suppliedPassword,
+      salt,
+      64,
+    )) as Buffer;
 
-    return buffer.toString('hex') === hashedPassword;
+    return timingSafeEqual(derivedKey, keyBuffer);
   }
 }
 
