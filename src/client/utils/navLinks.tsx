157b80d0c0f511923bc415b7f3a561795d5654f1
pprob <p.probert340@gmail.com>
2021-08-22 23:27:47 +0100

    feat: navlink data for rendering

--- src/client/utils/navLinks.tsx
+++ src/client/utils/navLinks.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faUser, faUserPlus} from '@fortawesome/free-solid-svg-icons';
+import {
+  faUser,
+  faUserPlus,
+  faShoppingCart,
+  faPowerOff,
+  faUserLock,
+} from '@fortawesome/free-solid-svg-icons';
 
 export enum NavLinkIcons {
   MyAccount = 'My Account',
+  SignIn = 'Sign in',
   LoggedInFilter = 'logged in',
   LoggedOutFilter = 'logged out',
   SignUp = 'Sign up',
+  SignOut = 'Sign out',
+  Cart = 'Cart',
 }
 
 export enum NavLinks {
@@ -14,6 +23,7 @@ export enum NavLinks {
   Catalog = 'Catalog',
   AboutUs = 'About Us',
   Blog = 'Blog',
+  Home = 'Home',
 }
 
 export const navigationIcons = [
@@ -22,12 +32,33 @@ export const navigationIcons = [
     text: NavLinkIcons.MyAccount,
     redirectUrl: '/my-account',
     filter: NavLinkIcons.LoggedInFilter,
-    order: 2,
+    order: 3,
   },
   {
     icon: <FontAwesomeIcon icon={faUserPlus} />,
     text: NavLinkIcons.SignUp,
     redirectUrl: '/signup',
+    filter: NavLinkIcons.LoggedOutFilter,
+    order: 2,
+  },
+  {
+    icon: <FontAwesomeIcon icon={faShoppingCart} />,
+    text: NavLinkIcons.Cart,
+    redirectUrl: '/my-basket',
+    filter: NavLinkIcons.LoggedInFilter,
+    order: 4,
+  },
+  {
+    icon: <FontAwesomeIcon icon={faPowerOff} />,
+    text: NavLinkIcons.SignOut,
+    redirectUrl: '/signout',
+    filter: null,
+    order: 5,
+  },
+  {
+    icon: <FontAwesomeIcon icon={faUserLock} />,
+    text: NavLinkIcons.SignIn,
+    redirectUrl: '/signin',
     filter: NavLinkIcons.LoggedOutFilter,
     order: 1,
   },
@@ -35,11 +66,18 @@ export const navigationIcons = [
 
 export const navigationLinks = [
   {
-    text: NavLinks.Products,
+    text: NavLinks.Home,
+    redirectUrl: '/',
     order: 1,
   },
   {
+    text: NavLinks.Products,
+    redirectUrl: '/products',
+    order: 2,
+  },
+  {
     text: NavLinks.AboutUs,
+    redirectUrl: '/about-us',
     order: 2,
   },
 ];
