b6788cb38bb2e76fda2a4e6fed0cc9090c3d5e98
pprob <p.probert340@gmail.com>
2021-08-22 15:58:29 +0100

    chore: setup jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/client/setuptests.ts'],
+  moduleNameMapper: {
+    // eslint-disable-next-line max-len
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.ts',
+  },
+};
