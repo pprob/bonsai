b0a756a431d817d05087b8ab954c97a0258f8b44
pprob <p.probert340@gmail.com>
2021-08-22 23:28:04 +0100

    feat: routes boilerplate

--- src/client/containers/App/Routes.tsx
+++ src/client/containers/App/Routes.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
-const Routes = () => {
+const Routes: React.FC = () => {
   return (
     <Switch>
-      <Route exact={true} path="/" render={() => <div>Home page</div>} />
+      <Route exact={true} path='/' render={() => <div>Home page</div>} />
+      <Route path='/signup' render={() => <div>Signup</div>} />
     </Switch>
   );
 };
