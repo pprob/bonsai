49509603ea48ed12ac52849bdc707f9de4b3e164
pprob <p.probert340@gmail.com>
2021-08-22 23:26:59 +0100

    feat: header updates

--- src/client/components/Header/index.tsx
+++ src/client/components/Header/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import NavIcon from '../NavIcon';
+import HamBurgerMenu from '../HamburgerMenu';
 import {navigationIcons, navigationLinks} from '../../utils/navLinks';
+import {push} from 'connected-react-router';
 import './index.scss';
+import {useDispatch} from 'react-redux';
 
 const Header: React.FC = () => {
+  const dispatch = useDispatch();
   const navLinksWithIcons = navigationIcons
     .filter(() => true)
     .sort((a, b) => a.order - b.order);
@@ -11,12 +15,23 @@ const Header: React.FC = () => {
 
   return (
     <div className='header__container'>
-      <div className='header-burger-container'></div>
+      <div className='header-burger-container'>
+        <HamBurgerMenu />
+        <div>Logo here</div>
+      </div>
       <div className='header-navigation-links'>
         <ul>
-          {navLinksNoIcons.map((links, index) => (
-            <li key={index}>{links.text}</li>
-          ))}
+          {navLinksNoIcons.map((links, index) => {
+            const handleOnClick = () => {
+              return dispatch(push(links.redirectUrl));
+            };
+
+            return (
+              <li key={index} onClick={handleOnClick}>
+                {links.text}
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className='header-navigation-icons'>
