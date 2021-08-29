9bb73570d2a2a7940a86fa751cb0c864654eb22f
pprob <p.probert340@gmail.com>
2021-08-29 16:36:11 +0100

    chore: add slider to App render

--- src/client/containers/App/index.tsx
+++ src/client/containers/App/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Routes from './Routes';
 import Header from '../../components/Header';
+import SliderMenu from '../../components/SliderMenu';
 import '../../styles/_global.scss';
 
 const App: React.FC = () => (
   <>
     <Header />
+    <SliderMenu />
     <Routes />
   </>
 );
