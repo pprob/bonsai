bdde606aab21af458afb4b80deef81022286bd89
pprob <p.probert340@gmail.com>
2021-08-29 19:21:44 +0100

    chore: wireup user router

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import {productsRouter} from './products';
+import {userRouter} from './user';
 
 const router = express.Router();
 
 router.use(productsRouter);
+router.use(userRouter);
 
 export {router as apiRouter};
