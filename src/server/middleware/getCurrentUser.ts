cdba097813e95f8b880e3ec2779cb7b7955662c4
pprob <p.probert340@gmail.com>
2021-08-29 19:18:11 +0100

    feat: get current user middleware

---
+++ src/server/middleware/getCurrentUser.ts
@@ -0,0 +1,37 @@
+import {Request, Response, NextFunction} from 'express';
+import jwt from 'jsonwebtoken';
+
+interface authUser {
+  email: string;
+  id: string;
+}
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      authUser?: authUser;
+    }
+  }
+}
+
+const getCurrentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
+  if (!req.session?.token) {
+    next();
+  }
+  try {
+    const authUser = jwt.verify(
+      req.session?.token,
+      process.env.JWT_SECRET!,
+    ) as authUser;
+    req.authUser = authUser;
+  } catch {}
+
+  next();
+};
+
+export default getCurrentUser;
