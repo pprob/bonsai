ea1fd6b802e0ef79763a7e66b704f6c681b8d4be
pprob <p.probert340@gmail.com>
2021-08-31 18:14:40 +0100

    chore: add multer middleware

--- src/server/api/products/index.ts
+++ src/server/api/products/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import createProduct from './handlers/createProduct';
 import getProducts from './handlers/getProduct';
+import upload from '../../middleware/imageFormDataMiddleware';
 
 const router = express.Router();
 
 router.get('/products', getProducts);
-router.post('/products', createProduct);
+router.post('/products', upload.array('products', 8), createProduct);
 
 export {router as productsRouter};
