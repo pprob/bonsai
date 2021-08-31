47934cd49cf5e59a7c8602dda6b0cfc38d9f6b01
pprob <p.probert340@gmail.com>
2021-08-31 13:23:13 +0100

    feat: update navlinks

--- src/client/utils/navLinks.tsx
+++ src/client/utils/navLinks.tsx
@@ -6,9 +6,11 @@ import {
   faShoppingCart,
   faPowerOff,
   faUserLock,
+  faHome,
+  faStoreAlt,
 } from '@fortawesome/free-solid-svg-icons';
 
-export enum NavLinkIcons {
+export enum NavLinkConstants {
   MyAccount = 'My Account',
   SignIn = 'Sign in',
   LoggedInFilter = 'logged in',
@@ -16,6 +18,8 @@ export enum NavLinkIcons {
   SignUp = 'Sign up',
   SignOut = 'Sign out',
   Cart = 'Cart',
+  sliderMenuUpper = 'Upper',
+  sliderMenuLower = 'Lower',
 }
 
 export enum NavLinks {
@@ -26,58 +30,89 @@ export enum NavLinks {
   Home = 'Home',
 }
 
+const myAccountLink = {
+  icon: <FontAwesomeIcon icon={faUser} />,
+  text: NavLinkConstants.MyAccount,
+  redirectUrl: '/my-account',
+  headerFilter: NavLinkConstants.LoggedInFilter,
+  sliderMenuFilter: NavLinkConstants.sliderMenuUpper,
+  order: 3,
+};
+
+const signupLink = {
+  icon: <FontAwesomeIcon icon={faUserPlus} />,
+  text: NavLinkConstants.SignUp,
+  redirectUrl: '/signup',
+  headerFilter: NavLinkConstants.LoggedOutFilter,
+  sliderMenuFilter: NavLinkConstants.sliderMenuLower,
+  order: 2,
+};
+
+const basketLink = {
+  icon: <FontAwesomeIcon icon={faShoppingCart} />,
+  text: NavLinkConstants.Cart,
+  redirectUrl: '/my-basket',
+  headerFilter: NavLinkConstants.LoggedInFilter,
+  sliderMenuFilter: NavLinkConstants.sliderMenuUpper,
+  order: 4,
+};
+
+const signOutLink = {
+  icon: <FontAwesomeIcon icon={faPowerOff} />,
+  text: NavLinkConstants.SignOut,
+  redirectUrl: '/signout',
+  headerFilter: NavLinkConstants.LoggedInFilter,
+  sliderMenuFilter: NavLinkConstants.sliderMenuLower,
+  order: 5,
+};
+
+const signInLink = {
+  icon: <FontAwesomeIcon icon={faUserLock} />,
+  text: NavLinkConstants.SignIn,
+  redirectUrl: '/signin',
+  headerFilter: NavLinkConstants.LoggedOutFilter,
+  sliderMenuFilter: NavLinkConstants.sliderMenuLower,
+  order: 1,
+};
+
+const homeLink = {
+  text: NavLinks.Home,
+  icon: <FontAwesomeIcon icon={faHome} />,
+  redirectUrl: '/',
+  order: 1,
+  sliderMenuFilter: NavLinkConstants.sliderMenuUpper,
+};
+
+const productsLink = {
+  text: NavLinks.Products,
+  icon: <FontAwesomeIcon icon={faStoreAlt} />,
+  redirectUrl: '/products',
+  order: 2,
+  sliderMenuFilter: NavLinkConstants.sliderMenuUpper,
+};
+
+const aboutUsLink = {
+  text: NavLinks.AboutUs,
+  redirectUrl: '/about-us',
+  order: 3,
+  sliderMenuFilter: NavLinkConstants.sliderMenuUpper,
+};
+
 export const navigationIcons = [
-  {
-    icon: <FontAwesomeIcon icon={faUser} />,
-    text: NavLinkIcons.MyAccount,
-    redirectUrl: '/my-account',
-    filter: NavLinkIcons.LoggedInFilter,
-    order: 3,
-  },
-  {
-    icon: <FontAwesomeIcon icon={faUserPlus} />,
-    text: NavLinkIcons.SignUp,
-    redirectUrl: '/signup',
-    filter: NavLinkIcons.LoggedOutFilter,
-    order: 2,
-  },
-  {
-    icon: <FontAwesomeIcon icon={faShoppingCart} />,
-    text: NavLinkIcons.Cart,
-    redirectUrl: '/my-basket',
-    filter: NavLinkIcons.LoggedInFilter,
-    order: 4,
-  },
-  {
-    icon: <FontAwesomeIcon icon={faPowerOff} />,
-    text: NavLinkIcons.SignOut,
-    redirectUrl: '/signout',
-    filter: null,
-    order: 5,
-  },
-  {
-    icon: <FontAwesomeIcon icon={faUserLock} />,
-    text: NavLinkIcons.SignIn,
-    redirectUrl: '/signin',
-    filter: NavLinkIcons.LoggedOutFilter,
-    order: 1,
-  },
+  myAccountLink,
+  signupLink,
+  basketLink,
+  signOutLink,
+  signInLink,
 ];
 
-export const navigationLinks = [
-  {
-    text: NavLinks.Home,
-    redirectUrl: '/',
-    order: 1,
-  },
-  {
-    text: NavLinks.Products,
-    redirectUrl: '/products',
-    order: 2,
-  },
-  {
-    text: NavLinks.AboutUs,
-    redirectUrl: '/about-us',
-    order: 2,
-  },
+export const navigationLinks = [homeLink, productsLink, aboutUsLink];
+
+export const sliderLinks = [
+  homeLink,
+  productsLink,
+  signOutLink,
+  signInLink,
+  basketLink,
+  myAccountLink,
 ];
