2059864e03ac7e17f621d048cc9f9e4ae0fc6f2a
pprob <p.probert340@gmail.com>
2021-09-07 19:26:31 +0100

    feat: auth input validators

---
+++ src/client/utils/inputValidators.ts
@@ -0,0 +1,39 @@
+/* eslint-disable max-len */
+
+const emailRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+const containsNumberRegex = /\d/;
+const containsSpecialCharacterRegex = /[-!$%^&*@()_+|~=`{}\[\]:";'<>?,.\/]/;
+
+const getError = (valid: boolean, message: string): authErrors => ({
+  isValid: valid,
+  errorMessage: message,
+});
+
+type authErrors = {
+  isValid: boolean;
+  errorMessage: string;
+};
+
+const isValidSignupForm = (
+  email: string,
+  password: string,
+  password2: string,
+): authErrors[] => [
+  getError(emailRegex.test(email), 'Please use a valid email address'),
+  getError(
+    containsNumberRegex.test(password),
+    'Password must contain a number',
+  ),
+  getError(
+    containsSpecialCharacterRegex.test(password),
+    `Password must contain atleast one of the following special characters !$%^&*@()[]:;'<>?,./`,
+  ),
+  getError(password === password2, 'Passwords must be matching'),
+];
+
+const isValidSigninForm = (email: string): authErrors[] => [
+  getError(emailRegex.test(email), 'Please enter a valid email address'),
+];
+
+export {isValidSignupForm, isValidSigninForm};
