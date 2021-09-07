22d09ccb64d4302ec5685195d63bcffb8ea5ad1c
pprob <p.probert340@gmail.com>
2021-09-07 19:34:57 +0100

    chore: products reducer

--- src/client/redux/reducers.ts
+++ src/client/redux/reducers.ts
@@ -2,11 +2,13 @@ import {History} from 'history';
 import {combineReducers, Reducer} from 'redux';
 import {connectRouter} from 'connected-react-router';
 import globalReducer from './global/reducer';
+import productsReducer from './products/reducer';
 
 const createRootReducer = (history: History): Reducer => {
   const appReducers = {
     router: connectRouter(history),
     global: globalReducer,
+    products: productsReducer,
   };
   return combineReducers(appReducers);
 };
