0f187ee3409c5040e564b0917db614a0904c7f9c
pprob <p.probert340@gmail.com>
2021-09-12 00:54:23 +0100

    feat: add signin route

--- src/client/containers/App/Routes.tsx
+++ src/client/containers/App/Routes.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import ProductLayout from '../Products';
+import SigninLayout from '../Signin';
 import SignupLayout from '../Signup';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route exact={true} path='/' render={() => <div>Home page</div>} />
-      <Route path='/signin' render={() => <div>Signin</div>} />
+      <Route path='/signin' component={SigninLayout} />
       <Route path='/signup' component={SignupLayout} />
       <Route path='/products' component={ProductLayout} />
       <Route render={() => <div>error page</div>} />
