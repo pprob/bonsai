84be6a280acbcf5ef6a7e4d6fc82f2365cafec83
pprob <p.probert340@gmail.com>
2021-09-14 23:26:10 +0100

    chore: resolve ts issues

--- src/client/redux/configureStore.ts
+++ src/client/redux/configureStore.ts
@@ -5,7 +5,7 @@ import {routerMiddleware} from 'connected-react-router';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './rootSaga';
 
-const configureStore = (history: History, initialState = {}): Store => {
+const configureStore = (history: any, initialState = {}): Store => {
   const sagaMiddleware = createSagaMiddleware();
 
   const middlewares: Middleware[] = [routerMiddleware(history), sagaMiddleware];
