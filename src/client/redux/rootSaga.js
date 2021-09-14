fb9fcd9379b03c00b8218d7afdc30d5c57766fb9
pprob <p.probert340@gmail.com>
2021-09-14 23:30:20 +0100

    feat: add sign in saga

--- src/client/redux/rootSaga.js
+++ src/client/redux/rootSaga.js
@@ -1,8 +1,9 @@
 import {fork} from '@redux-saga/core/effects';
 import {watchFetchProducts} from './products/sagas';
-import {watchSigninUser} from './authentication/sagas';
+import {watchSignupUser, watchSignInUser} from './authentication/sagas';
 
 export default function* rootSaga() {
   yield fork(watchFetchProducts);
-  yield fork(watchSigninUser);
+  yield fork(watchSignupUser);
+  yield fork(watchSignInUser);
 }
